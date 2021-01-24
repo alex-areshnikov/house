cb7291a780222d519118d22a1cd7faf14591a3be
Alex Areshnikov <alexander@decisely.com>
2021-01-23 17:07:22 -0700

    Dockerize sidekiq

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,20 +5,26 @@ cd /var/www/house
 
 RAILS_ENV=production
 
-# Run migrations
-bundle exec rake db:migrate
-
-# Run listener
-#bundle exec rake mqtt:listen_holodilnic_topic RAILS_ENV=production
-
 # Run Rails
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rails assets:clean
-bundle exec rails assets:precompile
-bundle exec rails s -b 0.0.0.0 -e production
+if [ ! -z $APP_CONTAINER ]; then
+  # Run migrations
+  bundle exec rake db:migrate
+
+  # Precompile assets
+  bundle exec rails assets:clean
+  bundle exec rails assets:precompile
+
+  # Run server
+  bundle exec rails s -b 0.0.0.0 -e production
+fi
+
+if [ ! -z $SIDEKIQ_CONTAINER ]; then
+   bundle exec sidekiq
+fi
 
 # exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
