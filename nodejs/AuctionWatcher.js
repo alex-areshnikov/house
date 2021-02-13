1d98fa6cb950925e4ff428bdcf33ded01f2084b6
Alex Areshnikov <alexander@decisely.com>
2021-02-12 20:25:44 -0700

    Auctioneer initial version

--- nodejs/AuctionWatcher.js
+++ nodejs/AuctionWatcher.js
@@ -1,31 +1,65 @@
-import puppeteer from "puppeteer-extra"
-import StealthPlugin from "puppeteer-extra-plugin-stealth"
-import HouseRedisClient from "./HouseRedisClient.js"
-import lotFields from "./lotFields.js"
+import HouseApiClient from "./HouseApiClient.js"
 
-puppeteer.use(StealthPlugin())
-
-export default class LotScanner {
-  constructor(lot_number) {
-    this.redis_client = new HouseRedisClient("auction_watcher");
+export default class AuctionWatcher {
+  constructor(logger, lot_number) {
+    this.logger = logger;
+    this.api_client = new HouseApiClient("auction_watcher");
     this.lot_number = lot_number;
     this.url = `https://www.copart.com/lot/${lot_number}`;
   }
 
-  watch = async () => {
+  watch = async (page) => {
     console.log(`Opening ${this.url}`)
 
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1920, height: 1080 });
     await page.goto(this.url, { waitUntil: "load" })
+    const lotExists = await page.waitForSelector('.lot-information')
+      .catch(() => { this.logger.warn("Lot not found") })
+
+    if(lotExists) {
+      await this.processAuction(page)
+    } else {
+      await page.screenshot({path: `screenshots/${Date.now()}_error_lot_${this.lot_number}_not_found.png`})
+    }
 
     // 1. find and click bid button
     // 2. check and close auction if vehicle has outdated number
     // 3. on lot change check if the lot is vehicle lot
     // 4. if so, watch and emit price
     // 5. otherwise go to 2
+  }
 
-    await browser.close()
+  processAuction = async (page) => {
+    const auctionButton = await page.waitForSelector('.live-auction-notification a')
+      .catch(() => { this.logger.warn("Auction not found") })
+
+    if(!auctionButton) return
+
+    auctionButton.click({ delay: 69 })
+
+    // TODO find iframe first
+
+    const actionPageExists = await page.waitForSelector('.widget')
+      .catch(() => { this.logger.warn("Auction page not found") })
+
+    if(actionPageExists) {
+      await this.reportAuctionLotNumber(page)
+    } else {
+      await page.screenshot({path: `screenshots/${Date.now()}_error_auction_page_${this.lot_number}_not_found.png`})
+    }
+  }
+
+  reportAuctionLotNumber = async (page) => {
+    const currentVehicleNumberElement = await page.waitForSelector('.auction-wrapper-MEGA [data-uname="lot-details-value"]')
+      .catch(() => { this.logger.warn("Current vehicle number not found") })
+
+    const targetVehicleNumberElement = await page.waitForSelector('.megaFutureLotHeaderWrapper [data-uname="lot-details-value"]')
+      .catch(() => { this.logger.warn("Target vehicle number not found") })
+
+    if(!currentVehicleNumberElement || !targetVehicleNumberElement) return
+
+    const currentVehicleNumber = currentVehicleNumberElement.evaluate(element => element.textContent.trim())
+    const targetVehicleNumber = targetVehicleNumberElement.evaluate(element => element.textContent.trim())
+
+    this.logger.say(`Auction page found. Current vehicle ${currentVehicleNumber} / Target vehicle ${targetVehicleNumber}`)
   }
 }
--- nodejs/CommandProcessor.js
+++ nodejs/CommandProcessor.js
@@ -1,23 +1,71 @@
 import { spawn } from "child_process";
 
-const scanLotCommand = "scan-lot";
+const KNOWN_COMMANDS = {
+  scanLotCommand: "scan-lot",
+  watchAuction: "watch-auction",
+  closeAuction: "close-auction"
+}
 
 export default class CommandProcessor {
   constructor(logger) {
     this.logger = logger;
+    this.openedAuctions = {};
   }
 
   process = async (data) => {
     switch (data.command) {
-      case scanLotCommand:
-        const scannerProcess = spawn("yarn", ["scan_lot", data.lot_number, data.username, data.password]);
+      case KNOWN_COMMANDS.scanLotCommand:
+        this.spawnScanLot(data);
 
-        scannerProcess.on('close', (code) => {
-          this.logger.say(`scan-lot-${data.lot_number} exited with code ${code}`);
-        });
+        break
+      case KNOWN_COMMANDS.watchAuction:
+        this.spawnWatchAuction(data);
+
+        break
+      case KNOWN_COMMANDS.closeAuction:
+        this.closeWatchAuction(data);
+
         break
       default:
         await this.logger.warn(`Unrecognized command ${data.command}.`);
     }
+  }
+
+  spawnScanLot = (data) => {
+    const scannerProcess = spawn("yarn", ["scan_lot", data.lot_number, data.username, data.password]);
+
+    scannerProcess.on('close', (code) => {
+      this.logger.say(`scan-lot-${data.lot_number} exited with code ${code}`);
+    });
+  }
+
+  spawnWatchAuction = (data) => {
+    if(this.openedAuctions[data.lot_number]) {
+      this.logger.warn(`Auctioneer is already spawned for ${data.lot_number}`);
+      return
+    }
+
+    const auctionProcess = spawn("yarn", ["watch_auction", data.lot_number, data.username, data.password]);
+
+    this.openedAuctions[data.lot_number] = auctionProcess
+
+    auctionProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    auctionProcess.on('close', (code) => {
+      this.logger.say(`watch-auction-lot-${data.lot_number} exited with code ${code}`);
+    });
+  }
+
+  closeWatchAuction = (data) => {
+    if(!this.openedAuctions[data.lot_number]) {
+      this.logger.warn(`Can't close. Auctioneer is not found for ${data.lot_number}`);
+      return
+    }
+
+    this.openedAuctions[data.lot_number].kill('SIGINT');
+
+    delete this.openedAuctions[data.lot_number]
   }
 }
