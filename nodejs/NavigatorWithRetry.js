d28b0c4890e5c6dc55fe8001efbedb99b1562473
Alex Areshnikov <alexander@decisely.com>
2021-03-30 15:25:56 -0600

    Capture screenshot on failed navigation

--- nodejs/NavigatorWithRetry.js
+++ nodejs/NavigatorWithRetry.js
@@ -1,14 +1,18 @@
 import ElementFinderWithRetry from "./ElementFinderWithRetry.js"
+import ApiLogger from "./ApiLogger.js";
+import UnexpectedPageStateReporter from "./UnexpectedPageStateReporter.js";
 
 export default class NavigatorWithRetry {
   constructor(url, selector) {
     this.url = url;
     this.elementFinderWithRetry = new ElementFinderWithRetry(selector)
+    this.logger = new ApiLogger('navigator-with-retry');
+    this.unexpectedPageStateReporter = new UnexpectedPageStateReporter(this.logger);
   }
 
   navigate = async (page) => {
-    await page.goto(this.url, { waitUntil: ["load", "domcontentloaded", "networkidle0", "networkidle2"]})
+    await page.goto(this.url, { waitUntil: ["load", "domcontentloaded", "networkidle0", "networkidle2"]}).catch(() => { this.error = true })
 
-    return await this.elementFinderWithRetry.find(page)
+    return(this.error ? null : (await this.elementFinderWithRetry.find(page)))
   }
 }
