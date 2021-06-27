d2a5c92a0a8d4bad59f3bf8d250b3e9357ffc08c
Alex Areshnikov <alexander@decisely.com>
2021-06-26 21:57:27 -0600

    Remove easing

--- app/javascript/packs/application_bottom.js
+++ app/javascript/packs/application_bottom.js
@@ -1,4 +1,2 @@
 import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min"
-import "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min"
-
 import "startbootstrap-sb-admin-2/js/sb-admin-2.min.js"
