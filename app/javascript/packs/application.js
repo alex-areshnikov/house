08c4b1a1446f5432bcdf44e1b95afd9c89dc269d
Alex Areshnikov <alexander@decisely.com>
2021-07-10 22:39:31 -0600

    Fix JS reload issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 
+import "startbootstrap-sb-admin-2/vendor/jquery/jquery.min"
+import "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min"
+import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min"
+import "startbootstrap-sb-admin-2/js/sb-admin-2.min.js"
+
 import "startbootstrap-sb-admin-2/css/sb-admin-2.min.css"
-import "startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css"
-
 import "../stylesheets/application.scss"
-
-const images = require.context('../images', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
