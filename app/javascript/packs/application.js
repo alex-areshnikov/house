2a831c31f1cd520d0217b9f7aed95b7f2b34f4ca
Alex Areshnikov <alexander@decisely.com>
2021-07-10 23:07:28 -0600

    Collapse sidebar by default

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("@rails/activestorage").start()
 import "startbootstrap-sb-admin-2/vendor/jquery/jquery.min"
 import "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min"
 import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min"
-import "startbootstrap-sb-admin-2/js/sb-admin-2.min.js"
 
 import "startbootstrap-sb-admin-2/css/sb-admin-2.min.css"
 import "../stylesheets/application.scss"
--- app/javascript/packs/application_bottom.js
+++ app/javascript/packs/application_bottom.js
@@ -1,2 +1,1 @@
-import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min"
 import "startbootstrap-sb-admin-2/js/sb-admin-2.min.js"
